40ce9f5b9f5a5660893be22e2ad5b3455a68382b
Caner Taş <canerrtass1@gmail.com>
2024-05-31 22:06:40 +0300

    deneme

--- cartService/src/main/java/com/turkcell/crm/cartService/clients/ProductServiceClient.java
+++ cartService/src/main/java/com/turkcell/crm/cartService/clients/ProductServiceClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "catalogservice",configuration = FeignClientConfiguration.class)
+@FeignClient(name = "localhost:9002",configuration = FeignClientConfiguration.class)
 public interface ProductServiceClient {
     @GetMapping("/api/v1/products/isExist/{id}")
     boolean isExist(@PathVariable int id);
